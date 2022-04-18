e8ab9cb86b1dd56bb0d7eac1a82a2f89778e0976
miguelzamorarey <miguelzamorarey@gmail.com>
2022-04-18 11:57:31 +0200

    feat: first commit

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Hi Mole!");
+// 1) NUMBER
+// Explicita
+let phone;
+phone = 123;
+//phone = '123'; ERROR
+//Inferido
+let phoneNumber = 123;
+//phoneNumber = ""; ERROR
+let hex = 0xf00d;
+let binary = 0b10110;
+let octal = 0o744;
+// 2) BOOLEAN
+let bool = true;
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+console.log("Hi Mole!");
+
+// 1) NUMBER
+// Explicita
+let phone : number;
+phone = 123;
+//phone = '123'; ERROR
+
+//Inferido
+let phoneNumber = 123;
+//phoneNumber = ""; ERROR
+
+let hex: number = 0xf00d;
+let binary: number = 0b10110;
+let octal: number = 0o744;
+
+// 2) BOOLEAN
+let bool: boolean = true;
